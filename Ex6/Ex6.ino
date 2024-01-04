14204478cd49b4444da8157667a4b80ea7640fff
s224350 <s224350@student.dtu.dk>
2024-01-04 11:58:32 +0100

    Exercise 6 FIXED

--- Ex6/Ex6.ino
+++ Ex6/Ex6.ino
@@ -3,7 +3,7 @@ const int b = 4;
 const int c = 6;
 const int d = 8;
 const int e = 10;
-int incomming = 0;
+int incomingByte = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,23 +13,30 @@ void setup() {
   pinMode(c, OUTPUT);
   pinMode(d, OUTPUT);
   pinMode(e, OUTPUT);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
-    incomming = Serial.read();
-    if ((char)incomming == 'a') {
+    incomingByte = Serial.read();
+    if ((char)incomingByte == 'a') {
       digitalWrite(a, HIGH);
-    } else if ((char)incomming == 'b') {
+    } else if ((char)incomingByte == 'b') {
       digitalWrite(b, HIGH);
-    } else if ((char)incomming == 'c') {
+    } else if ((char)incomingByte == 'c') {
       digitalWrite(c, HIGH);
-    } else if ((char)incomming == 'd') {
+    } else if ((char)incomingByte == 'd') {
       digitalWrite(d, HIGH);
-    } else if ((char)incomming == 'e') {
+    } else if ((char)incomingByte == 'e') {
       digitalWrite(e, HIGH);
-    } else{
+    } else if (incomingByte == 10) {
+      //all Serial inputs include a newline byte (decimal value 10), which would ordinarily count turn off all the lights if not for this condition.
+    } else {
       digitalWrite(a, LOW);
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
