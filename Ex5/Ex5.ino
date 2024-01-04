3529173eecf32639a8932ae9286edd2fd5c9c72f
s224350 <s224350@student.dtu.dk>
2024-01-04 11:03:37 +0100

    Exercise 5 part 2

--- Ex5/Ex5.ino
+++ Ex5/Ex5.ino
@@ -7,6 +7,8 @@ void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    Serial.print(incomingByte, DEC);
+    Serial.print(" or ");
+    Serial.println((char)incomingByte);
   }
 }
