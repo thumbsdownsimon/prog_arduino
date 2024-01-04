a9ec75b3f3bbca0de7883449a8f7e3b4f5f64d44
s224350 <s224350@student.dtu.dk>
2024-01-04 10:43:48 +0100

    Exercise 5

---
+++ Ex5/Ex5.ino
@@ -0,0 +1,12 @@
+int incomingByte = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
+}
