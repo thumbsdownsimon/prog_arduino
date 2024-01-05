5b896ac20335a5d292df1914c1fc0bcb0ee52fcf
s224350 <s224350@student.dtu.dk>
2024-01-05 10:29:45 +0100

    Exercise 10

---
+++ Ex10/Ex10.ino
@@ -0,0 +1,34 @@
+const int coldLed = 2;
+const int midLed = 4;
+const int warmLed = 6;
+const byte A0pin = A0;
+const double LM35 = 1023;
+
+void setup() {
+  pinMode(A0pin, INPUT);
+  pinMode(coldLed, OUTPUT);
+  pinMode(midLed, OUTPUT);
+  pinMode(warmLed, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int A0input = analogRead(A0pin);
+  double mVolt = A0input / LM35 * 5000; //convert it into millivolt since we know 10 millitvolt is 1° celcius
+  double C = mVolt / 10.0 + 2;//conver the millivolt into celsius
+  
+  if (C < 18) {
+    digitalWrite(coldLed, HIGH);
+    digitalWrite(midLed, LOW);
+    digitalWrite(warmLed, LOW);
+  } else if (C < 24) {
+    digitalWrite(coldLed, LOW);
+    digitalWrite(midLed, HIGH);
+    digitalWrite(warmLed, LOW);
+  } else {
+    digitalWrite(coldLed, LOW);
+    digitalWrite(midLed, LOW);
+    digitalWrite(warmLed, HIGH);
+  }
+
+}
