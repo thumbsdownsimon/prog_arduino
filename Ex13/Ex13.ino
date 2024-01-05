ff4cf19dfca52caa879340d566042a706239f61c
Simon <s224331@dtu.dk>
2024-01-05 14:49:05 +0100

    Ex 13 vid

--- Ex13/Ex13.ino
+++ Ex13/Ex13.ino
@@ -1,21 +1,13 @@
-int led1 = 2;
-int led2 = 4;
-int led3 = 6;
-int led4 = 8;
-int led5 = 10;
-const int speed = 5;
-const time = 100;
-void setup() {
-
-  Serial.begin(9600);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
+void state() {
+  int a[5] = { 9, 2, 42, 5, 8 };
+  int *pointer = &a[0];
 }
 
+
 void loop() {
-
-
+  Serial.printf("Address of pointer is %x\n", pointer);
+  Serial.printf("Value of pointer is %d\n\n", *pointer);
+  pointer++;
+  
+  delay(3000);
 }
