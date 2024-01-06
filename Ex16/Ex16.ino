f94b3e0553e240b8421ed5db694aedf7a72e53c9
s224350 <s224350@student.dtu.dk>
2024-01-06 14:42:18 +0100

    more exercise 16

--- Ex16/Ex16.ino
+++ Ex16/Ex16.ino
@@ -1,8 +1,19 @@
 void setup() {
   //fun with pointers
   Serial.begin(9600);
+
+  
+}
+
+void loop() {
+
+}
+
+void exercise1() {
+  //pointer to change the value of an integer variable
   int var = 5;
   int *ptrVar = &var;
+  
   Serial.print("Address of var is ");
   Serial.print((long)&var);
   Serial.print(", and value is ");
@@ -14,7 +25,7 @@ void setup() {
   Serial.println((long)ptrVar);
 
   *ptrVar = 10;
-
+  
   Serial.print("Address of var is ");
   Serial.print((long)&var);
   Serial.print(", and value is ");
@@ -24,7 +35,49 @@ void setup() {
   Serial.print((long)&ptrVar);
   Serial.print(", and value is ");
   Serial.println((long)ptrVar);
+}
+
+void exercise2() {
+  int a[5] = { 9, 2, 42, 5, 8 };
+  int *pointer = &a[0];
+  while (true) {
+    Serial.print("Address of pointer is ");
+    Serial.print((long)pointer);
+    Serial.print("Value of pointer is ");
+    Serial.println((long)*pointer);
+    pointer++;
+    ;
+    delay(3000);
+  }
+}
+
+void exercise3() {
+
+  int a = 5;
+  int b = 10;
+  int *ptrA = &a;
+  int *ptrB = &b;
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+
+  Serial.println("Swap!");
+  swap(ptrA, ptrB);
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
 
 }
 
-void loop() {}
+void swap(int *pointer_a, int *pointer_b) {
+  //swap the value of two integer variables
+  int temp = *pointer_a;
+
+  *pointer_a = *pointer_b;
+  *pointer_b = temp;
+
+}
