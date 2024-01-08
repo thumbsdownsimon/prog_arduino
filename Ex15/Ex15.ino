18144f1459377f483ffe33f5f95ecf5a3a717884
Simon <s224331@dtu.dk>
2024-01-08 12:46:21 +0100

    Ex15 revised

--- Ex15/Ex15.ino
+++ Ex15/Ex15.ino
@@ -1,33 +1,46 @@
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
+
 const char* ssid = "Simon";
 const char* pass = "Suiiiiiiii!";
 WiFiClient client;
-unsigned long channelID = 2397519;        //your TS channal
-const char* APIKey = "08JET6OE2DENHIWL";  //your TS API
+unsigned long channelID = 2397519;        // your TS channel
+const char* APIKey = "08JET6OE2DENHIWL";  // your TS API
 const char* server = "api.thingspeak.com";
-const int postDelay = 5 * 1000;  //post data every 20
+const int postDelay = 5 * 1000;  // post data every 20 seconds
 
-void handleLED() {  // If a POST request is made to URI /LED
-  //digitalWrite(led, !digitalRead(led));
+const int ledPin = D2;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  
+  pinMode(ledPin, OUTPUT);  // Initialize the LED pin as an output
 }
 
-  void setup() {
-    Serial.begin(115200);
-    WiFi.begin(ssid, pass);
-  }
-  float data;
+void loop() {
+  float data = WiFi.RSSI();  // Get the current RSSI value
+  Serial.print("RSSI: ");
+  Serial.println(data);
 
-  void loop() {
-    //data = 42.0;
-    data = WiFi.RSSI();
-    Serial.print("RSSI: ");
-    Serial.println(data);
-    ThingSpeak.begin(client);
-    client.connect(server, 80);    //connect(URL, Port)
-    ThingSpeak.setField(1, data);  //set data on the X
+  int ledState;
 
-    ThingSpeak.writeFields(channelID, APIKey);  //post everything to TS
-    client.stop();
-    delay(postDelay);  //wait and then post
+
+  if (data > -60) {
+    digitalWrite(ledPin, HIGH);
+    ledState = 1;
+  } else {
+    digitalWrite(ledPin, LOW);
+    ledState = 0;
   }
+
+  ThingSpeak.begin(client);
+  client.connect(server, 80);    //connect(URL, Port)
+  ThingSpeak.setField(1, data);  //set RSSI data on field 1
+  ThingSpeak.setField(2, ledState);  //set LED state on field 2
+
+  ThingSpeak.writeFields(channelID, APIKey);  //post everything to TS
+  client.stop();
+
+  delay(postDelay);  //wait and then post
+}
